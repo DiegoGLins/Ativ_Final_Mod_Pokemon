71c6fc590bc0aa84fe31ee279de76b52b5fedc06
Diego <dl7designer@gmail.com>
2023-12-13 20:08:51 -0300

    inclusao das cores por tipo de pokemon

--- src/utils/colorType.ts
+++ src/utils/colorType.ts
@@ -1,8 +1,14 @@
-import { NamedAPIResource } from "../types/PokemonType"
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-const colorType: NamedAPIResource = {
-    name: "",
-    url: ""
+const colorType: any = {
+    fire: { card: "#ff986b", chip: "#fff2db" },
+    grass: { card: '#5eff8b', chip: '#f2f2f2' },
+    water: { card: "#95beff", chip: "#007acc" },
+    normal: { card: "#c7d1d5", chip: "#f2f2f2" },
+    bug: { card: "#dc97ef", chip: "#eaf6fb" },
+    electric: { card: "#fff138", chip: "#b2a827" },
+    poison: { card: "#c0f5e5", chip: "#5eff8b" },
+    ground: { card: "#986d51", chip: "#e4a379" }
 }
 
 export default colorType
