239c5bbaafa8a0f41107313fa1138809c1e0f3c4
Diego <dl7designer@gmail.com>
2023-12-13 20:06:35 -0300

    alteracao no pokemonCard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -2,24 +2,26 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState } from 'react';
-import { Card, CardContent, CardMedia, Typography, Button, Grid, Snackbar, Alert, Box } from '@mui/material';
-import { Ability, PokemonSprites } from '../types/PokemonType';
+import { Card, CardContent, CardMedia, Typography, Button, Grid, Snackbar, Alert, Box, Chip } from '@mui/material';
+import { Ability, PokemonSprites, TypePokemon } from '../types/PokemonType';
 import ModalDetail from './ModalDetail';
 import StarIcon from '@mui/icons-material/Star';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { favorite } from '../store/modules/pokedexSlice/pokedexSlice';
+import colorType from '../utils/colorType';
 
 interface Pokedex {
     id: number;
     name: string;
     height: number;
     abilities: Ability[];
+    types: TypePokemon[];
     sprites: PokemonSprites
 }
-
 interface PokemonCardProps {
     pokemon: Pokedex;
 }
+
 
 const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
     const dispatch = useAppDispatch();
@@ -39,34 +41,38 @@ const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
             dispatch(favorite(pokemon));
         }
     };
-
     function handleClose() {
         setOpenModal(false)
     }
 
+    const cardColors: any = colorType[pokemon.types[0].type.name] ? colorType[pokemon.types[0].type.name] : colorType.normal
+    console.log(colorType)
     return (
         <>
             <ModalDetail isOpen={openModal} actionCancel={() => handleClose()} pokemon={pokemon} />
             <Box sx={{ display: 'flex', width: '300px', height: '500px', margin: '15px', }}>
-                <Card sx={{ paddingTop: '15px', backgroundColor: 'antiquewhite', width: '300px', height: '490px' }}>
+                <Card sx={{ paddingTop: '15px', backgroundColor: cardColors?.card || colorType.normal, width: '300px', height: '490px' }}>
                     <Button onClick={handlePokedex} sx={{ color: `${isPokemonInPokedex ? '#eac625' : '#ddd8dd'}`, alignSelf: 'end', display: 'flex', paddingInlineStart: '15px' }}>
                         <StarIcon sx={{ padding: '3px', borderRadius: '100%', width: '21px', height: '21px', backgroundColor: `${isPokemonInPokedex ? "#000" : "#d58318"}` }} className='favoritePokemon' />
                     </Button>
                     <CardMedia sx={{ borderRadius: '20px' }}
                         component="img"
                         alt={pokemon?.name}
-                        height="270"
-                        width='250'
+                        height="250"
+                        width='240'
                         image={pokemon?.sprites.front_default} />
                     <CardContent>
-                        <Typography variant="body1" color='primary.main'>
-                            <strong>{pokemon?.name}</strong>
+                        <Typography variant="body1" color='primary.dark'>
+                            <strong>{pokemon?.name.toUpperCase()}</strong>
                         </Typography>
-                        <Typography variant="body1" color="text.secondary">
-                            ID: {pokemon?.id}
+                        <Typography variant="body1" color="primaray.dark">
+                            <strong>ID:</strong> {pokemon?.id}
                         </Typography>
-                        <Typography variant="body1" color="secondary.contrastText">
-                            Height: {pokemon?.height}
+                        <Typography variant="body1" color="primaray.dark">
+                            <strong>Height:</strong> {pokemon?.height}
+                        </Typography>
+                        <Typography sx={{ marginTop: '10px' }} variant="body1" >
+                            <Chip sx={{ borderRadius: '5px', backgroundColor: `${cardColors?.chip}`, color: '#000' }} label={pokemon.types[0].type.name} />
                         </Typography>
                         <Grid item xs={12} sx={{ padding: '15px 0px 0px 0px' }} justifyContent={'center'} alignItems={'center'}>
                             <Button onClick={() => setOpenModal(true)} variant='contained'>Detalhes</Button>
